7c7915c8975cd662ab9cf4fdd713c48089d8eb96
Anastassiya Prokudina <anakess88@gmail.com>
2023-03-14 11:31:27 +0600

    Изменила представление для 'Корзины'
    Поменяла модель для представления на "Продукт" для удобства обращения к полям.

--- online_store/urls.py
+++ online_store/urls.py
@@ -1,21 +1,33 @@
 from django.urls import path
 
 from online_store.views.base import IndexView, IndexRedirectView
-from online_store.views.cart import CartView
+from online_store.views.cart import CartView, ProductCartCreateView
 from online_store.views.categories import *
 from online_store.views.products import *
 
-
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("product/", IndexRedirectView.as_view(), name='products_index_redirect'),
+    path("product/", IndexRedirectView.as_view(), name="products_index_redirect"),
     path("product/create", ProductCreateView.as_view(), name="product_create"),
     path("product/<int:pk>", ProductDetail.as_view(), name="product_detail"),
-    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
-    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
-    path("product/<int:pk>/confirm_delete/", ProductDeleteView.as_view(), name="product_confirm_delete"),
+    path(
+        "product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"
+    ),
+    path(
+        "product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"
+    ),
+    path(
+        "product/<int:pk>/confirm_delete/",
+        ProductDeleteView.as_view(),
+        name="product_confirm_delete",
+    ),
     path("product/products_delete", products_delete, name="products_delete"),
     path("category/", categories_list, name="categories_list"),
     path("products/<pk>", category, name="category"),
-    path("cart/", CartView.as_view(), name="cart_list")
+    path("cart/", CartView.as_view(), name="cart_list"),
+    path(
+        "product/<int:pk>/cart/add/",
+        ProductCartCreateView.as_view(),
+        name="product_cart_add",
+    ),
 ]
--- online_store/views/cart.py
+++ online_store/views/cart.py
@@ -1,14 +1,44 @@
-from django.views.generic import ListView
+from django.db.models import Q, F, Sum
+from django.shortcuts import redirect
+from django.views.generic import ListView, TemplateView
 
-from online_store.models import ProductInCart
+from online_store.models import Cart, Product
 
 
 class CartView(ListView):
-    template_name = 'products/cart.html'
-    model = ProductInCart
-    context_object_name = 'cart'
-    ordering = ('id',)
+    template_name = "cart/cart.html"
+    model = Product
+    context_object_name = "products"
+    ordering = ("id",)
 
     def get_queryset(self):
-        queryset = super().get_queryset().filter(qty__gt=0)
+        queryset = (
+            super()
+            .get_queryset()
+            .filter(Q(carts__qty__gt=0))
+            .annotate(total=F("carts__qty") * F("price"))
+        )
         return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["result"] = Product.objects.get_total_result()
+        return context
+
+
+class ProductCartCreateView(TemplateView):
+    model = Cart
+    template_name = "products/index.html"
+
+    def post(self, request, **kwargs):
+        product_in_cart = Cart.objects.filter(product_id=kwargs["pk"])
+        if product_in_cart:
+            balance = Product.objects.get_balance(pk=kwargs["pk"])
+            qty = Cart.objects.get_qty(product_id=kwargs["pk"])
+            if balance >= qty:
+                product_in_cart.update(qty=qty)
+            else:
+                message = f"Unable to add to cart. Only {balance}pcs in stock."
+        else:
+            Cart.objects.create(product_id=kwargs["pk"], qty=1)
+        return redirect("index")
